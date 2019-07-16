18e23d4770685be7645c00d3b2a874d6f4f980ce
NKN <knguyentnh98@gmail.com>
2019-07-16 16:12:12 +0700

    fix image

--- datasetTool/run_dataset.sh
+++ datasetTool/run_dataset.sh
@@ -1,2 +1,2 @@
-python3 datasetTool.py -v ./rawData/vu-huy-hop.mp4 -n ./dataset/vu-huy-hop
+# python3 datasetTool.py -v ./rawData/vu-huy-hop.mp4 -n ./dataset/vu-huy-hop
 python3 datasetTool.py -v ./rawData/dang-lam-tung.mp4 -n ./dataset/dang-lam-tung
